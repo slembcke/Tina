a6c0f797b0a497cd2ef06ebfe5c65ab5dfb6f09c
slembcke <slembcke@gmail.com>
2019-08-07 12:48:23 -0500

    Simplify.

--- tina.asm
+++ tina.asm
@@ -7,45 +7,20 @@ extern _abort
 %define ARG1 rsi
 %define RET rax
 
-%macro push_regs 0
-	push rbp
-	push rbx
-	push r12
-	push r13
-	push r14
-	push r15
-%endmacro
-
-%macro pop_regs 0
-	pop r15
-	pop r14
-	pop r13
-	pop r12
-	pop rbx
-	pop rbp
-%endmacro
-
-%macro swap_rsp 1
-	mov rdx, rsp
-	mov rsp, [%1 + 8]
-	mov [%1 + 8], rdx
-%endmacro
-
 global _tina_wrap
 _tina_wrap: ; (tina* coro, tina_func* body)
-	; Push body() to the stack and yield.
-	mov rbp, rsp
-	push ARG0
-	push ARG1
+	; Save 'coro' and 'body' to preserved registers.
+	mov r12, ARG0
+	mov r13, ARG1
 	call _tina_yield
 	
 	; Run the coroutine body.
-	mov ARG0, [rbp - 8]
+	mov ARG0, r12
 	mov ARG1, RET
-	call [rbp - 16]
+	call r13
 	
 	; Yield the coroutine's return value.
-	mov ARG0, [rbp - 8]
+	mov ARG0, r12
 	mov ARG1, RET
 	call _tina_yield
 	
@@ -56,11 +31,27 @@ global _tina_resume
 global _tina_yield
 _tina_yield: ; (tina* coro, uintptr_t value)
 _tina_resume: ; (tina* coro, uintptr_t value)
-	push_regs
-	swap_rsp ARG0
-	pop_regs
+	; Preserve calling coroutine registers.
+	push rbp
+	push rbx
+	push r12
+	push r13
+	push r14
+	push r15
+	
+	; Swap stacks.
+	mov rdx, rsp
+	mov rsp, [ARG0 + 8]
+	mov [ARG0 + 8], rdx
+	
+	; Restore callee coroutine registers.
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop rbx
+	pop rbp
 	
 	; Move 'value' to return register, pop and jump to the resume address.
 	mov RET, ARG1
-	pop rdx
-	jmp rdx
+	ret
