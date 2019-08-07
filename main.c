1680abdaad5e6344396b0f07c1e1aa103720d9f8
slembcke <slembcke@gmail.com>
2019-08-07 15:41:14 -0500

    Some error handling.

--- main.c
+++ main.c
@@ -7,28 +7,37 @@
 
 typedef struct tina tina;
 typedef uintptr_t tina_func(tina* coro, uintptr_t value);
+typedef void tina_err_func(void);
+
+tina_err_func* tina_err;
 
 struct tina {
 	void* ctx;
 	
 	void* _rsp;
-	alignas(16) uint8_t _stack[1024*1024];
+	alignas(16) uint8_t _stack[];
 };
 
 uintptr_t tina_wrap(tina* coro, uintptr_t value);
 uintptr_t tina_resume(tina* coro, uintptr_t value);
 uintptr_t tina_yield(tina* coro, uintptr_t value);
 
-tina* tina_new(tina_func* body, void* ctx){
-	tina* coro = calloc(1, sizeof(tina));
+void err(const char* message){
+	fprintf(stderr, "Tina err: %s\n", message);
+}
+
+tina* tina_new(size_t size, tina_func* body, void* ctx){
+	tina* coro = calloc(1, sizeof(tina) + size);
 	coro->ctx = ctx;
 	
-	// Setup the stack to call tina_wrap().
-	uintptr_t* rsp = (uintptr_t*)(coro->_stack + sizeof(coro->_stack));
+	uintptr_t* rsp = (uintptr_t*)(coro->_stack + size);
+	// Push the wrapper function address onto the stack.
 	*(--rsp) = (uintptr_t)tina_wrap;
+	// Push zeros onto the stack for the saved registers.
 	rsp -= 6;
 	coro->_rsp = rsp;
 	
+	// Start the wrapper function.
 	tina_resume(coro, (uintptr_t)body);
 	
 	return coro;
@@ -48,12 +57,15 @@ static uintptr_t coro_body(tina* coro, uintptr_t value){
 	return false;
 }
 
-void foobar(void);
-
 int main(int argc, const char *argv[]){
-	tina* coro = tina_new(coro_body, NULL);
-	while(tina_resume(coro, 0)){}
+	tina_err = err;
 	
-	printf("success\n");
+	tina* coro = tina_new(16*1024, coro_body, NULL);
+	while(tina_resume(coro, 0)){}
+	printf("Success!\n");
+	
+	printf("Resuming again will crash...\n");
+	tina_resume(coro, 0);
+	
 	return EXIT_SUCCESS;
 }
--- tina.asm
+++ tina.asm
@@ -1,36 +1,48 @@
+extern _tina_err
 extern _abort
 
 ; System V rgument order: RDI, RSI, RDX, RCX, R8, and R9
 ; return value: RAX
-
 %define ARG0 rdi
 %define ARG1 rsi
 %define RET rax
 
 global _tina_wrap
 _tina_wrap: ; (tina* coro, tina_func* body)
+%push
+%define %$coro r12
+%define %$body r13
+
 	; Save 'coro' and 'body' to preserved registers.
-	mov r12, ARG0
-	mov r13, ARG1
+	mov %$coro, ARG0
+	mov %$body, ARG1
 	call _tina_yield
 	
 	; Run the coroutine body.
-	mov ARG0, r12
+	mov ARG0, %$coro
 	mov ARG1, RET
-	call r13
+	call %$body
 	
 	; Yield the coroutine's return value.
-	mov ARG0, r12
+	mov ARG0, %$coro
 	mov ARG1, RET
 	call _tina_yield
 	
 	; Crash if the coroutine is resumed after exiting.
-	jmp _abort
+	mov rax, [qword _tina_err]
+	test rax, rax
+	jz .abort
+	mov ARG0, .err_complete
+	call rax
+	
+	.abort: call _abort
+	
+	.err_complete: db "Attempted to resume a completed coroutine.",0
+%pop
 
-global _tina_resume
-global _tina_yield
-_tina_yield: ; (tina* coro, uintptr_t value)
+global _tina_resume, _tina_yield
 _tina_resume: ; (tina* coro, uintptr_t value)
+_tina_yield:
 	; Preserve calling coroutine registers.
 	push rbp
 	push rbx
