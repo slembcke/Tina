bad9ae2c3a6018ec8d45fb7423ee408e427dfb0d
slembcke <slembcke@gmail.com>
2020-01-17 17:25:30 -0600

    Simplify.

--- tina.c
+++ tina.c
@@ -1,30 +1,35 @@
-#include "stdbool.h"
-#include "stdlib.h"
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include "tina.h"
 
 // TODO: Are there any relevant ABIs that aren't 16 byte aligned, downward moving stacks?
 // TODO: Is it worthwhile to try and detect stack overflows?
 
-// Defined in assembly.
-tina* tina_context(tina* coro, void** sp_loc, void* sp, tina_func* body);
+tina* tina_init_stack(tina* coro, tina_func* body, void** sp_loc, void* sp);
 uintptr_t tina_swap(tina* coro, uintptr_t value, void** sp);
 
 tina* tina_init(void* buffer, size_t size, tina_func* body, void* user_data){
 	tina* coro = buffer;
 	(*coro) = (tina){.user_data = user_data, .running = true};
-	return tina_context(coro, &coro->_sp, buffer + size, body);
+	return tina_init_stack(coro, body, &coro->_sp, buffer + size);
 }
 
 uintptr_t tina_yield(tina* coro, uintptr_t value){
 	return tina_swap(coro, value, &coro->_sp);
 }
 
-// Function called when a coroutine body function exits.
-void tina_finish(tina* coro, uintptr_t value){
+void tina_context(tina* coro, tina_func* body){
+	// Yield back to the tina_init_stack() call, and return the coroutine.
+	uintptr_t value = tina_yield(coro, (uintptr_t)coro);
+	// Call the body function with the first value.
+	value = body(coro, value);
+	// body() has exited, and the coroutine is finished.
 	coro->running = false;
+	// Yield the final return value back to the calling thread.
 	tina_yield(coro, value);
 	
-	// Any attempt to resume the coroutine after it's dead should call the error func.
+	// Any attempt to resume the coroutine after it's finished should call the error func.
 	while(true){
 		if(coro->error_handler) coro->error_handler(coro, "Attempted to resume a dead coroutine.");
 		tina_yield(coro, 0);
@@ -39,9 +44,8 @@ asm(".intel_syntax noprefix");
 #define ARG3 "rcx"
 #define RET "rax"
 
-asm(".global tina_context");
-asm(".func tina_context");
-asm("tina_context:");
+asm(".func tina_init_stack");
+asm("tina_init_stack:");
 // Save the caller's registers and stack pointer.
 // tina_yield() will restore them once the coroutine is primed.
 asm("  push rbp");
@@ -50,30 +54,21 @@ asm("  push r12");
 asm("  push r13");
 asm("  push r14");
 asm("  push r15");
-asm("  mov ["ARG1"], rsp");
-// save 'coro' and 'body' to preserved registers.
-asm("  mov rbp, "ARG0"");
-asm("  mov rbx, "ARG3"");
+asm("  mov ["ARG2"], rsp");
 // Align and apply the coroutine's stack.
-asm("  and "ARG2", ~0xF");
-asm("  mov rsp, "ARG2"");
+asm("  and "ARG3", ~0xF");
+asm("  mov rsp, "ARG3"");
+// Now executing within the new coroutine's stack!
+// When tina_context() first calls tina_yield() it will
+// return back to where tina_init_stack() was called.
+
 // Push an NULL activation record onto the stack to make debuggers happy.
 asm("  push 0");
 asm("  push 0");
-// The coroutine is now primed. Yield the coroutine pointer back to tina_init().
-asm("  mov "ARG1", "ARG0"");
-asm("  call tina_yield");
-// Pass the initial tina_yield() value on to body().
-asm("  mov "ARG0", rbp");
-asm("  mov "ARG1", "RET"");
-asm("  call rbx");
-// Tail call tina_finish() with the final value from body().
-asm("  mov "ARG0", rbp");
-asm("  mov "ARG1", "RET"");
-asm("  jmp tina_finish");
+// Tail call to tina_context() to finish the coroutine initialization.
+asm("  jmp tina_context");
 asm(".endfunc");
 
-asm(".global tina_swap");
 asm(".func tina_swap");
 asm("tina_swap:");
 // Preserve calling coroutine's registers.
